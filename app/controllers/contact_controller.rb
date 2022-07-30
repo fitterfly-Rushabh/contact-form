498657640715e09fc65a013f01e3b11ed5ff3f7a
Divyesh Patel <pateldivyesh009@gmail.com>
2022-07-30 15:24:22 +0530

    changes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,5 @@
 class ContactController < ApplicationController
+  http_basic_authenticate_with :name => "name", :password => "password", only: :list
   def index
     Enquiry.create(name: params[:name], email: params[:email], message: params[:message])
   end
